9b76d681e497c7abca67a02986633152b6ecb8aa
maxim <maddmax22@yandex.ru>
2024-09-18 11:05:12 +0300

    add additional methods to close modal window

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ModalContent from '../modal-content/index';
 import './style.css';
 
@@ -9,8 +9,31 @@ const Modal = ({
   show,
   totalCost,
 }) => {
+  const modalRef = useRef(null);
+
+  const handleClickHide = ({ target }) => {
+    if (target === modalRef.current) {
+      onHide();
+    }
+  };
+
+  const handleEscClose = ({ key }) => {
+    if (key === 'Escape') {
+      onHide();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', (e) => handleClickHide(e));
+    window.addEventListener('keydown', (e) => handleEscClose(e));
+    return () => {
+      window.removeEventListener('click', handleClickHide);
+      window.removeEventListener('keydown', handleEscClose);
+    }
+  }, [show]);
+
   return (
-    <div className={show ? 'Modal' : 'visually-hidden'}>
+    <div ref={modalRef} className={show ? 'Modal' : 'visually-hidden'}>
       <ModalContent list={list} onDelete={onDelete} totalCost={totalCost} onHide={onHide} />
     </div>
   );
