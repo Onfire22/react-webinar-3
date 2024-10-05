e435bf5d54297f383ae3da7f38fc97d1fb8672b6
maxim <maddmax22@yandex.ru>
2024-10-05 16:52:03 +0300

    fix login redirect

--- src/app/login/index.js
+++ src/app/login/index.js
@@ -4,13 +4,21 @@ import LoginForm from "../../components/login-form";
 import useSelector from "../../hooks/use-selector";
 import useStore from "../../hooks/use-store";
 import Navigation from "../../containers/navigation";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Login = () => {
   const store = useStore();
+  const navigate = useNavigate();
   const select = useSelector((state) => ({
     name: state?.user?.userData?.name,
+    loggedIn: state?.user?.loggedIn,
     error: state?.user?.error,
   }));
+
+  useEffect(() => {
+    if (select.loggedIn) navigate(-1);
+  }, []);
 
   const logIn = (data) => store.actions.user.logIn(data);
 
